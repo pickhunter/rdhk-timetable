72c37985a74ea24037423ece72d3fe31d1f7fd87
Rohit Garg <rohhittt@gmail.com>
2015-10-22 09:24:30 +0530

    Removing schedule

--- app/models/schedule.rb
+++
@@ -1,6 +0,0 @@
-class Schedule < ActiveRecord::Base
-  belongs_to :batch
-  belongs_to :time_table
-
-  validates :name, presence: true
-end
--- config/routes.rb
+++ config/routes.rb
@@ -6,7 +6,6 @@ Rails.application.routes.draw do
   resources :departments
   resources :teachers
   resources :rooms
-  resources :schedules
   resources :slots
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
