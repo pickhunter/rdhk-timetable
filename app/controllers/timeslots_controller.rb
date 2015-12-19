2870aa491f167302f20701b610a9fb66b345fcd4
Rohit Garg <rohhittt@gmail.com>
2015-12-19 14:08:49 +0530

    Fixing index view

--- app/controllers/timeslots_controller.rb
+++ app/controllers/timeslots_controller.rb
@@ -2,18 +2,31 @@ class TimeslotsController < ApplicationController
 	before_action :confirm_logged_in
   def index
   	@timeslots = Timeslot.all
-  	@monday = @timeslots.where("day = '0'") 
-  	@tuesday = @timeslots.where("day = '1'") 
-  	@wednesday = @timeslots.where("day = '2'") 
-  	@thursday = @timeslots.where("day = '3'") 
-  	@friday = @timeslots.where("day = '4'") 
-  	@saturday = @timeslots.where("day = '5'") 
+
+    if params[:subject_id] && params[:batches]
+      filtered_timeslots = @timeslots
+      params[:batches].each do |batch_id|
+        filtered_timeslots = filtered_timeslots - Batch.find(batch_id).timeslots
+      end
+
+      filtered_timeslots = filtered_timeslots - Subject.find(params[:subject_id]).timeslots
+
+      @timeslots = filtered_timeslots
+    end
+
+  	# @monday = @timeslots.where("day = '0'") 
+  	# @tuesday = @timeslots.where("day = '1'") 
+  	# @wednesday = @timeslots.where("day = '2'") 
+  	# @thursday = @timeslots.where("day = '3'") 
+  	# @friday = @timeslots.where("day = '4'") 
+  	# @saturday = @timeslots.where("day = '5'") 
   	year1 = params[:year].to_i
   	puts year1
     sem= (2*year1)-1
   	@batches = Batch.all.where("year = ?", year1)
     deptid=Department.find_by_code(session[:username]).id;
     @subjects = Subject.all.where("department_id = ?", deptid).where("semester = ?",sem)
+
   end
 
   def returnbatches
