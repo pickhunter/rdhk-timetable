7e8d5446eb5e015e1fbe33ce2305e2c8e09d69f4
Rohit Garg <rohhittt@gmail.com>
2015-12-19 14:08:32 +0530

    Fixing models

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,6 +1,7 @@
 class Batch < ActiveRecord::Base
-has_many :batch_subjects
-has_many :subjects , :through => :batch_subjects
-has_many :timeslots , :through => :slots
+  has_many :batch_subjects
+  has_many :subjects , :through => :batch_subjects
+  has_many :slots
+  has_many :timeslots , :through => :slots
   validates :name, presence: true
 end
